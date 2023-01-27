2020f5c67cc69a8ade0a9ebfe6ce19d7ed7347f9
Arunika Srivastava <aru26srivastava001@gmail.com>
2023-01-27 18:41:15 +0530

    Update Main.java

--- Linked_List/Linked_List/src/arunika/Main.java
+++ Linked_List/Linked_List/src/arunika/Main.java
@@ -1,16 +1,19 @@
 package arunika;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args1) throws Exception {
         LL list = new LL();
-        list.InsertFirst(3);
+        list.InsertFirst(1);
         list.InsertFirst(2);
-        list.InsertFirst(8);
-        list.InsertFirst(17);
-        list.InsertLast(99);
-
-        
+        list.InsertFirst(3);
+        list.InsertFirst(4);
+        list.InsertFirst(5);
+        list.display();
         list.InsertAtIndex(77, 3);
+        int element = list.DeleteFirst();
+
+        list.DeleteAtIndex(3);
+
         list.display();
     }
 }
